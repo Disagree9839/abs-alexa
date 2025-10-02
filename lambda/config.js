efa51e219803ec9361ba7702480e21db674e51d6
Disagree9839 <justinmhickman@rocketmail.com>
2025-10-02 19:46:50 -0400

    Update config.js

--- lambda/config.js
+++ lambda/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    ABS_API_KEY: 'xxxxxx',
-    SERVER_URL: 'https://abs.domain.tld',
+    ABS_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXlJZCI6ImRiNjBhOTQ1LWMzNDUtNDgyMC1iNzQ0LWE4MTI2NjkyNWUwOCIsIm5hbWUiOiJhYnMtYWxleGEgVFQgYWxleGEiLCJ0eXBlIjoiYXBpIiwiaWF0IjoxNzU5NDQ4Nzg2fQ.y1yWixh0O2L-qbYz0c6g5_VMRm1qDbEUX2ehrzIRCtI',
+    SERVER_URL: 'https://ab.banana.family',
     USER_AGENT: 'AlexaSkill'
     // OTHER OPTIONAL HEADERS
     // CFAccessClientId: 'xxxxxxxxx.access',
